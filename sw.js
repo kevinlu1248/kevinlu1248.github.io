afa144557668aa1b83f78110367b2823837eba54
Kevin Lu <kevinlu1248@gmail.com>
2021-06-20 13:50:46 -0700

    updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-91803621dc1e13a14510.js"
+    "url": "webpack-runtime-172d28663085f0b695f4.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a71b3f8e7dd4bf423c124c39072dc87d"
+    "revision": "b77b48a967c98ddba0883c3c3bd8c4c7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
